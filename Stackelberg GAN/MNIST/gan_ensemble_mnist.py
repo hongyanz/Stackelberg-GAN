24d52cccfd73b0bc8114bef3ba696a9ddfc6aae1
Hongyang Zhang <hongyanz@cs.cmu.edu>
2018-12-28 16:34:53 -0500

    Update gan_ensemble_mnist.py

--- Stackelberg GAN/MNIST/gan_ensemble_mnist.py
+++ Stackelberg GAN/MNIST/gan_ensemble_mnist.py
@@ -21,7 +21,7 @@ shutil.rmtree('images_ensemble_mnist')
 os.makedirs('images_ensemble_mnist', exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--n_epochs', type=int, default=500, help='number of epochs of training')
 parser.add_argument('--batch_size', type=int, default=100, help='size of the batches')
 parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
 parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
